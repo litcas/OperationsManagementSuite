fe3d981a0068806b7c4ec8d57b50aeb40a75ce9e
HanChangMing <han.changming@qq.com>
2017-11-13 11:37:13 +0800

    Initial commit

---
+++ src/main/java/com/rengu/operationsoanagementsuite/OperationsoanagementsuiteApplication.java
@@ -0,0 +1,12 @@
+package com.rengu.operationsoanagementsuite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationsoanagementsuiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperationsoanagementsuiteApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rengu/operationsoanagementsuite/OperationsoanagementsuiteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rengu.operationsoanagementsuite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OperationsoanagementsuiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
